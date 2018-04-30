6405c4bd52bbe2394abfa17622f110705efd53ba
katai5plate <katai5plate.sub@gmail.com>
2018-04-30 14:33:27 +0900

    Ran ESLint

--- src/show.js
+++ src/show.js
@@ -1,5 +1,5 @@
-import FizzBuzz from "./index";
+import FizzBuzz from "./index"
 FizzBuzz(process.argv[2] >> 0 || 100)
-    .forEach((v, i) => {
-        console.log(`  ${i + 1}:\t${v}`);
-    });
+  .forEach((v, i) => {
+    console.log(`  ${i + 1}:\t${v}`)
+  })
