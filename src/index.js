9b26d2e5b82ef2b721ef6d2bbd89bac686290ad0
katai5plate <katai5plate.sub@gmail.com>
2018-05-02 19:43:50 +0900

    Added Exception handing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import { _ } from 'lodash';
 
-export default v => _.range(1, v + 1)
-  .reduce((p, c) => {
-    let a = p;
-    const f = c % 3 === 0;
-    const b = c % 5 === 0;
-    if (f && b) a = [...a, 'fizzbuzz'];
-    else if (f) a = [...a, 'fizz'];
-    else if (b) a = [...a, 'buzz'];
-    else a = [...a, c];
-    return a;
-  }, []);
+export default (v) => {
+  let val = v;
+  if (v === undefined) val = 100;
+  else if (Number.isNaN(Number(v))) {
+    val = 0;
+  } else if (Number(val) < 0) {
+    throw new Error();
+    return [];
+  } else {
+    val = parseInt(v, 10);
+  }
+
+  return _.range(1, val + 1)
+    .reduce((p, c) => {
+      let a = p;
+      const f = c % 3 === 0;
+      const b = c % 5 === 0;
+      if (f && b) a = [...a, 'fizzbuzz'];
+      else if (f) a = [...a, 'fizz'];
+      else if (b) a = [...a, 'buzz'];
+      else a = [...a, c];
+      return a;
+    }, []);
+};
--- src/show.js
+++ src/show.js
@@ -1,6 +1,6 @@
 import FizzBuzz from './index';
 
-FizzBuzz(parseInt(process.argv[2], 10) || 100)
+FizzBuzz(process.argv[2])
   .forEach((v, i) => {
     console.log(`  ${i + 1}:\t${v}`);
   });
